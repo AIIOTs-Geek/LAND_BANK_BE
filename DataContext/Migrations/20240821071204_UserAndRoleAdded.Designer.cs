2b63bb7430d99e79e9eb3e565dd5fb040050f698
iflashera <iflashera0001@gmail.com>
2024-08-21 12:45:26 +0530

    User And Role Model Added

--- DataContext/Migrations/20240821071204_UserAndRoleAdded.Designer.cs
+++ DataContext/Migrations/20240821071204_UserAndRoleAdded.Designer.cs
@@ -4,6 +4,7 @@ using DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataContext.Migrations
 {
     [DbContext(typeof(LBDbContext))]
-    partial class LBDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240821071204_UserAndRoleAdded")]
+    partial class UserAndRoleAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
