4f5dbae094c1bff5c6d5f99f3ea1f999b4fca5df
iflashera <iflashera0001@gmail.com>
2024-08-29 15:08:56 +0530

    Minor Changes Added in GetLandByAssetId

--- Common/ViewModels/LandByAssetIdVm.cs
+++ Common/ViewModels/LandByAssetIdVm.cs
@@ -12,7 +12,7 @@ namespace Common.ViewModels
         public string ReferenceNumber { get; set; }
         public string AssetName { get; set; }
         public string SubAssetName { get; set; }
-        public string LandUse {  get; set; }
+        public string LandUse { get; set; }
         public string LandType { get; set; }
         public string LandStatus { get; set; }
         public string WLTStatus { get; set; }
@@ -25,5 +25,22 @@ namespace Common.ViewModels
         public string DeedStatus { get; set; }
         public string BusinessPlan { get; set; }
         public int? Status { get; set; }
+
+
+        public string StatusName
+        {
+            get
+            {
+                return Status switch
+                {
+                    1 => "Not Submit",
+                    2 => "Approved",
+                    3 => "Send Back",
+                    4 => "Waiting for Approval",
+                   
+                };
+            }
+
+        }
     }
 }
