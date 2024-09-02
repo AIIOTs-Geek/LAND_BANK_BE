34baa13acc27de0d2577529b3641b794109a6f7e
sabiya-rashid <bhatsabiya2@gmail.com>
2024-09-02 15:14:16 +0530

    Minor changes added

--- Common/Dtos/LandDto.cs
+++ Common/Dtos/LandDto.cs
@@ -9,32 +9,33 @@ namespace Common.Dtos
 {
     public class LandDto
     {
-        public int LandId { get; set; }
-        public int SubAssetId { get; set; }
-        public int DistrictId { get; set; }
+        public string? ActionName { get; set; }
+        public int? LandId { get; set; }
+        public int? SubAssetId { get; set; }
+        public int? DistrictId { get; set; }
         [Column(TypeName = "decimal(18, 2)")]
-        public decimal Area { get; set; }
-        public int BusinessPlanId { get; set; }
-        public int BusinessPlanStatusId { get; set; }
-        public int BusinessPlanDetailedId { get; set; }
-        public int LandStatusId { get; set; }
-        public int LandUseId { get; set; }
-        public int LandTypeId { get; set; }
-        public int WltStatus { get; set; }
-        public int PlotNumber { get; set; }
-        public string LandInfo { get; set; }  
+        public decimal? Area { get; set; }
+        public int? BusinessPlanId { get; set; }
+        public int? BusinessPlanStatusId { get; set; }
+        public int? BusinessPlanDetailedId { get; set; }
+        public int? LandStatusId { get; set; }
+        public int? LandUseId { get; set; }
+        public int? LandTypeId { get; set; }
+        public int? WltStatus { get; set; }
+        public int? PlotNumber { get; set; }
+        public string? LandInfo { get; set; }  
         public string Location { get; set; }
-        public string MapUrl { get; set; }
-        public int MasterPlan { get; set; }
-        public int InfraApproval { get; set; }
-        public int InfraContraction { get; set; }
-        public int MunHandingOver { get; set; }
-        public int DeedStatus { get; set; }
-        public string DeedNumber { get; set; }
-        public string DeedDate { get; set; }
-        public int DeedOwner { get; set; }
-        public int DeedType { get; set; }
-        public string DeedUrl { get; set; }
-        public int DeedSequence { get; set; }
+        public string? MapUrl { get; set; }
+        public int? MasterPlan { get; set; }
+        public int? InfraApproval { get; set; }
+        public int? InfraContraction { get; set; }
+        public int? MunHandingOver { get; set; }
+        public int? DeedStatus { get; set; }
+        public string? DeedNumber { get; set; }
+        public string? DeedDate { get; set; }
+        public int? DeedOwner { get; set; }
+        public int? DeedType { get; set; }
+        public string? DeedUrl { get; set; }
+        public int? DeedSequence { get; set; }
     }
 }
