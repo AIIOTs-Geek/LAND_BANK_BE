3389673adc8d1763d17d0a41db2890a22f02b132
sabiya-rashid <bhatsabiya2@gmail.com>
2024-09-07 16:15:50 +0530

    update land changed

--- Common/Dtos/UpdateLandDto.cs
+++ Common/Dtos/UpdateLandDto.cs
@@ -9,7 +9,6 @@ namespace Common.Dtos
 {
     public class UpdateLandDto
     {
-        public string? ActionName { get; set; }
         public int? LandId { get; set; }
         public int? SubAssetId { get; set; }
         public int? DistrictId { get; set; }
@@ -29,14 +28,6 @@ namespace Common.Dtos
         public int? MasterPlan { get; set; }
         public int? InfraApproval { get; set; }
         public int? InfraContraction { get; set; }
-        public int? MunHandingOver { get; set; }
-        public int? TitleDeedId { get; set; }
-        public int? DeedStatus { get; set; }
-        public string? DeedNumber { get; set; }
-        public string? DeedDate { get; set; }
-        public int? DeedOwner { get; set; }
-        public int? DeedType { get; set; }
-        public string? DeedUrl { get; set; }
-        public int? DeedSequence { get; set; }
+        public int? MunHandingOver { get; set; }        
     }
 }
