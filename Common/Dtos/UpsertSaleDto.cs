4118a8fda159fd130188a281d9bd5b1141c9672c
sabiya-rashid <bhatsabiya2@gmail.com>
2024-09-15 12:51:01 +0530

    dto fixed again

--- Common/Dtos/UpsertSaleDto.cs
+++ Common/Dtos/UpsertSaleDto.cs
@@ -9,14 +9,14 @@ namespace Common.Dtos
     public class UpsertSaleDto
     {
         public int? LandId { get; set; }
-        public string ?ReferenceNumber { get; set; }
+        public string? ReferenceNumber { get; set; }
         public int? SalesMethodId { get; set; }
         public int? SalesStatusId { get; set; }
         public decimal? Commission { get; set; }
         public decimal? SaleValue { get; set; }
         public DateTime? SaleDate { get; set; }
         public int? SalesRepresentative { get; set; }
-        public int AgentName { get; set; }
-        public int ?BuyerId { get; set; }
+        public int? AgentName { get; set; }
+        public int? BuyerId { get; set; }
     }
 }
